46252977c5904d852ffec8d9ad8fcdf85c1ea368
Puvvl <vasylputra@hotmail.com>
2020-11-19 13:53:26 +0200

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    initial
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
